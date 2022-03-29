3990db532268049c3558b128b188d4f40b043fe5
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-29 14:23:05 +0200

    added data, root colors, new vue init

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('JS OK');
+console.table(contacts);
+
+
+const app = new Vue({
+    el: '#boolzapp',
+
+    data: {
+        contacts,
+        
+
+    },
+    methods: {
+  
+
+    }
+});
