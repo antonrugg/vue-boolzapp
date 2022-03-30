2e69f0e6996cdbbd2a848ad289b51bef8fe827a6
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-30 09:25:02 +0200

    added click user active class

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ console.log('JS OK');
 console.table(contacts);
 
 
+
 const app = new Vue({
     el: '#boolzapp',
 
     data: {
         contacts,
-        index: 0
+        index: 0,
+        activeContact: null
+        
 
     },
     methods: {
@@ -15,6 +18,10 @@ const app = new Vue({
             this.activeContact = i;
         },
 
+        imgURLUser: function (index) {
+            return `img-bolzapp/avatar${contacts[index].avatar}.jpg`;
+        }
 
+        
     }
 });
